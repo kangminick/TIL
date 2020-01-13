0dedc7697cea51ed27c608b2218362c479973dc0
Kangmin <58929362+kangminick@users.noreply.github.com>
2020-01-13 17:47:04 +0900

    study

---
+++ python/basic_01.py
@@ -0,0 +1,13 @@
+array = [1,2,3, "four", "five",  "six", True]
+print(array)
+print(array[0:3])
+print(array[4:])
+print(array[-1])
+
+dust = {'seoul' : 50, 'incheon' : 40}
+print(dust)
+print(dust['seoul'])
+
+dust_two = dict(seoul=50)
+print(dust_two)
+print(dust_two['seoul'])
